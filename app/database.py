95e24a2bf863febeaaa91b434d58cc6d7ab135d5
zqyong <yongkingston@gmail.com>
2024-12-12 23:16:30 +0800

    fix db connection for compose

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@db/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
